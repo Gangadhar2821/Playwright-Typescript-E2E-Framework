6ac93a7cae4fb73cd4060b77364e99cdb74aac99
CRAFTSILICON0\gangadhar.b <gangadhar.b@craftsilicon.com>
2026-08-04 15:43:08 +0530

    Updated the flaky tests

--- fixtures/Basetest.ts
+++ fixtures/Basetest.ts
@@ -2,8 +2,10 @@ import { test as base, expect } from '@playwright/test';
 import { ConfigReader } from '../utils/ConfigReader';
 
 base.beforeEach(async ({ page }) => {
+  //Navigating to Test URL
   await page.goto(ConfigReader.get("BASE_URL"));
   await page.waitForLoadState('load');
+  //Verifying the landing page
   await expect(page.getByAltText('Website for automation practice')).toBeVisible({ timeout: 10000 });
   await page.getByRole('link', { name: /Signup \/ Login/i }).click();
   await page.waitForURL('**/login', { timeout: 10000 });
