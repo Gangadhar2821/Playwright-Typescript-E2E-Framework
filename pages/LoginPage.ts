0d747dc2d452a786aec50a6e8f8665a6a20e2d1d
CRAFTSILICON0\gangadhar.b <gangadhar.b@craftsilicon.com>
2026-08-04 15:50:42 +0530

    Updated the tests

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -9,13 +9,14 @@ export class LoginPage {
     readonly passwordTxt: Locator;
     readonly loginBtn: Locator;
     constructor(private page: Page) {
-        this.emailTxt = this.page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address');
+        this.emailTxt = this.page.getByRole('textbox', { name: 'email' });
         this.passwordTxt = this.page.getByRole('textbox', { name: 'Password' });
         this.loginBtn = this.page.getByRole('button', { name: 'Login' });
     }
 
 
     async login() {
+        await this.page.waitForLoadState('load');
         //Enter correct email address and password
         await this.emailTxt.fill(ConfigReader.get('EMAIL'));
         await this.passwordTxt.fill(ConfigReader.get('PASSWORD'));
@@ -24,7 +25,7 @@ export class LoginPage {
     }
 
     async login_invalidCreds(credentials: userdetails) {
-       
+        await this.page.waitForLoadState('load');
         await this.emailTxt.fill(credentials.email_invalid);
         await this.passwordTxt.fill(credentials.password_invalid);
         //Click 'login' button
