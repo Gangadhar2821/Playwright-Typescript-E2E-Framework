8c4175a3a47a1a0c17a9ed2c6dbc38404c511c93
CRAFTSILICON0\gangadhar.b <gangadhar.b@craftsilicon.com>
2026-08-04 17:31:23 +0530

    Updated tests

--- fixtures/Basetest.ts
+++ fixtures/Basetest.ts
@@ -4,8 +4,9 @@ import { ConfigReader } from '../utils/ConfigReader';
 base.beforeEach(async ({ page }) => {
   //Navigating to Test URL
   await page.goto(ConfigReader.get("BASE_URL"));
+  await page.waitForLoadState('load');
   //Verifying the landing page
-  await expect(page.getByAltText('Website for automation practice')).toBeVisible({ timeout: 30000 });
+  await expect(page).toHaveURL('https://automationexercise.com/');
   await page.getByRole('link', { name: /Signup \/ Login/i }).click();
   await expect(page.getByText('Login to your account')).toBeVisible({ timeout: 10000 });
 });
